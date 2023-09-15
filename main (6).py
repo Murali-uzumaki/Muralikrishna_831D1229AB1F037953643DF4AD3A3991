c236db802abb1770bf62e42568ecc2346f1f524d
Murali-uzumaki <144193317+Murali-uzumaki@users.noreply.github.com>
2023-09-15 13:20:38 +0530

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,15 @@
+# cricket player
+
+class Player:
+   def play(self):
+          print("Theplayerisplayingcricket.")
+class Batsman(Player):
+   def play(self):
+          print("Thebatsmanisbatting.")
+class Bowler(Player):
+   def play(self):
+          print("Thebowlerisbowling.")
+batsman=Batsman()
+bowler=Bowler()
+batsman.play()
+bowler.play()
